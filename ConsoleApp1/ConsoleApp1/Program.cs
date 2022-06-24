7367ffbd05a905b5e2c0efb0ad9a04db66670962
Yshk92 <falconblaze55@gmail.com>
2022-06-24 23:14:21 +0300

    this is my class work

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string charactername ="Leo";
+int heroheight = 180 + 5;
+    int heroage = 30;
+   string herosuperpower = "HollyLight";
+string vilianname ="Morgoth";
+int vilianage = 120;
+int vilianheight = 210;
+string viliansuperpower = "none";
+
+int agedifference = heroage - vilianage;
+Console.WriteLine("my age " + heroage + " my name " + charactername + " my height " + heroheight + " my super power " + herosuperpower);
+Console.WriteLine(" vilian name " + vilianname + " vilian age " + vilianage + " vilian height " + vilianheight + " vilian super power " + viliansuperpower);
